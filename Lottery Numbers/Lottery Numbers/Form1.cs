4a3963629cb571525b5b260bf93baf60c0149b72
Categio <categio1@gmail.com>
2022-03-18 16:36:09 -0700

    update lottery numbers program to add button functionality.

---
+++ Lottery Numbers/Lottery Numbers/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lottery_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            //create array to hold the numbers
+            const int Size = 5;
+            int[] lotteryNumbers = new int [Size];
+
+            //create Random object
+            Random rand = new Random();
+
+            //fill array with randomized numbers in range 0-99
+            for (int index = 0; index < lotteryNumbers.Length; index++)
+            {
+                lotteryNumbers[index] = rand.Next(100);
+            }
+            //Display array elements in Label controls
+            firstLabel.Text = lotteryNumbers[0].ToString();
+            secondLabel.Text = lotteryNumbers[1].ToString();
+            thirdLabel.Text = lotteryNumbers[2].ToString();
+            fourthLabel.Text = lotteryNumbers[3].ToString();
+            fifthLabel.Text = lotteryNumbers[4].ToString();
+            
+            
+        }
+        //close the form
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
